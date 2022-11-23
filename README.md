e051005d7e382888eb97a37ca36b3d0b00d21ff1
talitaspadaccini <tspadaccini1990@gmail.com>
2022-11-23 18:58:52 -0300

    feat: busca no trends

--- README.md
+++ README.md
@@ -4,3 +4,8 @@ Automated video production project.
 # Criar user-input
 Instalar biblioteca readline-sync
 
+git status
+git diff
+git add -A
+git commit -m 'feat: descrição commit'
+git push origin branch
--- index.js
+++ index.js
@@ -1,24 +1,43 @@
 const readline = require('readline-sync') // biblioteca que permite que sejam informados dados ao sistema utilizando-se da linha de comando.
+const Parser = require('rss-parser');
 
 // Função que agrupa tudo
-function start(){
+const TREND_URL = 'https://trends.google.com/trends/trendingsearches/daily/rss?geo=BR'
+
+async function start(){
     const content = {};
 
-    content.searchTerm = askAndReturnSearchTerm(); // Perguntar pelo termo de busca
+    content.searchTerm = await askAndReturnSearchTerm();
     content.prefix = askAndReturnPrefix(); // Pergunta pelo prefixo
 
     // Ao executar a função será injetado uma propriedade nova no objeto content
     // Vão retornar a string inserida e colocar o valor no searchTerm
-    function askAndReturnSearchTerm(){
+    async function askAndReturnSearchTerm(){
         // retorna método 'question' da biblioteca readline, que será injetado no content.searchTerm pela função
-        return readline.question('Digite o termo a ser buscado na Wikipedia: ');
+        const response = readline.question('Digite o termo a ser buscado na Wikipedia o digite G para busca no Google Trends:');
+                
+        return (response.toUpperCase() === 'G') ? await askAndReturnTrend() : response;
     }
 
+    async function askAndReturnTrend(){
+        console.log('Por favor, aguarde...');
+        const trends = await getGoogleTrends();
+        const choice = readline.keyInSelect(trends, 'Escolha a trend: ');
+
+        return trends[choice];
+    }
+
+    async function getGoogleTrends(){
+        const parser = new Parser();
+        const trends = await parser.parseURL(TREND_URL);
+        return trends.items.map(({title}) => title);
+    }
+    
     // Retorna prefixo que será utilizado para fazer um título no YT mais completo
     function askAndReturnPrefix(){
         // método 'keyInSelect' - select de opções que retorna uma chave selecionada pelo usuário como resultado.
         // consiste num array 
-        const prefixes = ['Quem é','Oque é', 'A história do', 'A história da'];
+        const prefixes = ['Quem é','Oque é', 'A história de'];
         const selectedPrefixIndex = readline.keyInSelect(prefixes, 'Escolha uma opção: ');
         const selectedPrefixText = prefixes[selectedPrefixIndex];
 
